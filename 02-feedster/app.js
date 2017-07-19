3d9360e7f8b0872d5e0e65baf4242bcdf00488d4
JovanaBajat <jovanabajat@yahoo.com>
2017-07-19 12:23:02 +0200

    add app.js feedster

---
+++ 02-feedster/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$('.notification').click(function() {
+		$('.notification-menu').toggle(300)
+	});
+
+	$('.btn').click(function() {
+		$(this).toggleClass("btn-like")
+		 
+		 
+	});
+	});
+
+
