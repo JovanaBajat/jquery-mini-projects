2defc3e83083fd52b06fd7fb73f4ea5deefac82d
JovanaBajat <jovanabajat@yahoo.com>
2017-07-19 11:12:20 +0200

    app.js event

---
+++ 00-pocketbook/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	$('form').submit(function(event) {
+		event.preventDefault();
+		var firstName = $('#first').val();
+		var lastName = $('#last').val();
+		var email = $('#email').val();
+		var password = $('#password').val();
+		  
+
+
+		//vérifier la valeur de l'input first-name
+		//si c'est vide, j'affiche msg erreur
+
+if (firstName === "") {
+	$('.first-name-error').html('Please enter your first name');
+};
+		//vérifier la valeur de l'input last-name
+		//si c'est vide, j'affiche msg erreur
+
+ if (lastName === "") {
+ 	$('.last-name-error').html('Please enter your last name');
+ };    
+
+		//vérifier la valeur de l'input email
+		//si c'est vide, j'affiche msg erreur
+
+if (email === "") {
+	$('.email-error').html('Please enter your email');
+};
+
+		//vérifier la valeur de l'input password
+		//si c'est vide, j'affiche msg erreur
+		//si password length < 8, j'affiche msg erreur
+
+if (password === "" ) {
+	$('.password-error').html('Please enter your password'); 
+};
+
+if (password.length < 8) {
+	$('.password-error').html('Password must have minimum 8 characters');
+};
+	});
+	});
